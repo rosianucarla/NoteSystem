c68f2ed85fde14da5b8d4d44562fc56274a50ba9
rosianucarla <61684170+rosianucarla@users.noreply.github.com>
2022-08-23 17:35:10 +0300

    Update NotesystemApplication.java
    main edited

--- notesystem/src/main/java/com/example/notesystem/NotesystemApplication.java
+++ notesystem/src/main/java/com/example/notesystem/NotesystemApplication.java
@@ -1,13 +1,28 @@
 package com.example.notesystem;
 
+import com.example.notesystem.model.Note;
+import com.example.notesystem.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotesystemApplication {
+public class NotesystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesystemApplication.class, args);
 	}
 
+	@Autowired
+	private NoteRepository noteRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	//	Note note1=Note.builder()
+	//	.name("note1")
+	//	.content("content1")
+	//	.build();
+	}
 }
