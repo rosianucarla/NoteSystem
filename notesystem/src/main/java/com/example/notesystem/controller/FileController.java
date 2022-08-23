01689e229f12f1fd09bc56f8ddfb2d78f2343b9d
rosianucarla <61684170+rosianucarla@users.noreply.github.com>
2022-08-23 17:34:41 +0300

    Update FileController.java
    file edited

--- notesystem/src/main/java/com/example/notesystem/controller/FileController.java
+++ notesystem/src/main/java/com/example/notesystem/controller/FileController.java
@@ -1,27 +1,57 @@
 package com.example.notesystem.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 @RestController
 public class FileController {
-    @RequestMapping(value = "/upload", method = RequestMethod.POST,
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
-    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
-        File convertFile = new File("/var/tmp/"+file.getOriginalFilename());
-        convertFile.createNewFile();
-        FileOutputStream fout = new FileOutputStream(convertFile);
-        fout.write(file.getBytes());
-        fout.close();
-        return "File is upload successfully";
+
+    @RequestMapping(value = "/notes", method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<String> writeFile() throws Exception {
+
+        String line = "";
+        try {
+            File file = new File("notes.txt");
+
+            //
+            //If the file does not exist, it generates
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            // Write and close file
+            bw.write(line);
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<String>(line, HttpStatus.CREATED);
+    }
+
+
+    @RequestMapping(value = "/notes", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> readArchive() throws Exception {
+
+        String line = null;
+        try {
+            FileReader ler = new FileReader("notes.txt");
+            BufferedReader reader = new BufferedReader(ler);
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<String>(line, HttpStatus.OK);
     }
 }
